9f347e9d61e6d12b074fae9263c2f0b5433a515f
TonyUCLA <TonyUCLA>
2017-02-20 22:11:09 -0800

    Skeletion and DB setup

---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Requiring the Schema
+var Articles = require("./models/Articles");
+// Inital Port
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./public"));
+
+// MongoDB Config
+mongoose.connect("mongodb://localhost/NYT-react-search");
+var db = mongoose.connection;
+
+// Error Handling
+db.on("error", function(error) {
+    console.log("Mongoose Error on ", error);
+});
+
+// On Success
+db.once("open", function() {
+    console.log("Connection to Mongoose Successful.");
+});
+
+
+
+
+// Main Route Here
